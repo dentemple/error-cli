4d8fc86e82fa8db58777297c8584634c353cc2f6
Reynolds A. Colon <reynolds@Reynoldss-MacBook-Pro.local>
2018-09-07 18:35:30 -0400

    edit server from get to post request

--- server/server.js
+++ server/server.js
@@ -3,6 +3,6 @@ const app = express();
 const UserController = require("./UserController");
 
 app.get("/", (req, res) => res.send("up and running"));
-app.get("/api/add", UserController.addUser);
+app.post("/api/add", UserController.addUser);
 
 app.listen(3000, () => console.log("listening on port 3000"));
