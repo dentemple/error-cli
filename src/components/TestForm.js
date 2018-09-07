e2467ead8734cc4da9788e148ac85c7480fd9a11
Reynolds A. Colon <reynolds@Reynoldss-MacBook-Pro.local>
2018-09-07 18:39:18 -0400

    update form to fetch data to and from database

--- src/components/TestForm.js
+++ src/components/TestForm.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import FormLabel from './FormLabel';
+import fetch from 'isomorphic-fetch';
+import '../../public/stylesheet/signup.css';
 
 export default class TestForm extends Component {
   constructor(props){
     super(props);
     this.state = {
       name: "",
-      password: ""
+      password: "",
+      savedSearches: null
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -29,49 +32,45 @@ export default class TestForm extends Component {
     if (formData.formSender.length < 1 || formData.formSecret.length < 1) {
       return false
     }
-    console.log({formData});
-    // $.ajax({
-    //  url: '/api/add',
-    //  dataType: 'json',
-    //  type: 'POST',
-    //  data: formData,
-    //  success: function(data) {
-    //    console.log('success case:', {data});
-    //   if (confirm('Thank you for your data!')) {
-    //     this.setState({
-    //      name: '',
-    //      password: ''
-    //     })
-    //   }
-    //  },
-    //  error: function(xhr, status, err) {
-    //   console.error(status, err.toString())
-    //   alert('There was some problem with sending your data.')
-    //  }
-    // })
-    const url = '/api/add'
-    fetch(url)
+    console.log('THIS IS FORM DATA THAT SHOULD SEND', {formData});
+    const url = '/api/add';
+    const { username, password, savedSearches } = this.state;
+    const client = { username };
+    const secret = { password };
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: {
+        username: JSON.stringify(client),
+        password: JSON.stringify(secret),
+      },
+    })
     .then( res => {
       return res.json()
     })
-    .then( json => {
-      console.log({json});
-      this.setState({
-        name: '',
-        password: ''
-      })
+    .then((json) => {
+      console.log('THIS IS DATA BEING RECEIVED', json);
     })
     .catch(err => console.log({err}));
-
   }
 
+  handleUsername (event){
+    this.setState({username: event.target.value});
+  }
+
+  handlePass (event) {
+    this.setState({password: event.target.value});
+  }
 
   render(){
     return(
+      <div className='fullForm'>
       <form onSubmit={this.handleSubmit}>
-
+        <h1 className='heading'>Welcome, Sign Up Here...</h1>
         <fieldset className='form-group'>
-          <FormLabel htmlFor='formName' title='Full Name:' />
+          <FormLabel className='form-label' htmlFor='formName' title='Username: ' />
           <input
             id='formName'
             className='form-input'
@@ -79,27 +78,29 @@ export default class TestForm extends Component {
             type='text'
             required onChange={this.handleChange}
             value={this.state.name}
+            placeholder='Enter Username Here...'
           />
         </fieldset>
 
         <fieldset className='form-group'>
-          <FormLabel htmlFor='formPass' title='Password:' />
+          <FormLabel className='form-label' htmlFor='formPass' title='Password: ' />
           <input
-            id='formName'
+            id='formPass'
             className='form-input'
             name='password'
             type='password'
             required onChange={this.handleChange}
             value={this.state.password}
+            placeholder='Enter Password Here...'
           />
         </fieldset>
 
         <div className='form-group'>
-          <input id='formButton' type='submit'/>
+          <input onClick={this.handleSubmit} id='formButton' type='submit'/>
         </div>
 
       </form>
-      // <button onClick={this.clickedBtn}>Our First Test</button>
+    </div>
     )
   }
 }
