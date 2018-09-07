afbd2f1581a0b1341637676d6e3dc5461a5e673d
Reynolds A. Colon <reynolds@Reynoldss-MacBook-Pro.local>
2018-09-07 10:29:18 -0400

    create button

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import TestBtn from './TestBtn';
+
 
 export default class App extends Component {
 	render() {
 		return (
 			<div>
-				<h1>Hello World!</h1>
+				<h1>Get Drunk On Errors!</h1>
+				<TestBtn/>
 			</div>
 		)
 	}
