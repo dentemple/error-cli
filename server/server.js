100140b8af3e71c6703c81fc07b8eb5fafcb1eb5
Joel Pratt <pratt.joel@gmail.com>
2018-09-07 19:36:20 -0400

    Get auth token from Github oauth.

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const app = express();
 const UserController = require("./UserController");
 
 app.get("/", (req, res) => res.send("up and running"));
-app.get("/api/add", UserController.addUser);
+app.post("/api/add", UserController.addUser);
+// app.get("/getUser", UserController.getUser);
+// app.get("/savedSearches", UserController.getSearches);
+app.get("/login", UserController.authenticateUser);
+app.get("/oauth", UserController.handleAthenticatedUser);
 
 app.listen(3000, () => console.log("listening on port 3000"));
