365044278febe139d1e49d37eede3086ffac4af5
Joel Pratt <pratt.joel@gmail.com>
2018-09-07 14:45:55 -0400

    Connect webpack and express server.

--- src/components/TestForm.js
+++ src/components/TestForm.js
@@ -28,31 +28,41 @@ export default class TestForm extends Component {
 
     if (formData.formSender.length < 1 || formData.formSecret.length < 1) {
       return false
-     }
+    }
+    console.log({formData});
+    // $.ajax({
+    //  url: '/api/add',
+    //  dataType: 'json',
+    //  type: 'POST',
+    //  data: formData,
+    //  success: function(data) {
+    //    console.log('success case:', {data});
+    //   if (confirm('Thank you for your data!')) {
+    //     this.setState({
+    //      name: '',
+    //      password: ''
+    //     })
+    //   }
+    //  },
+    //  error: function(xhr, status, err) {
+    //   console.error(status, err.toString())
+    //   alert('There was some problem with sending your data.')
+    //  }
+    // })
+    const url = '/api/add'
+    fetch(url)
+    .then( res => {
+      return res.json()
+    })
+    .then( json => {
+      console.log({json});
+      this.setState({
+        name: '',
+        password: ''
+      })
+    })
+    .catch(err => console.log({err}));
 
-     $.ajax({
-      url: '/api/add',
-      dataType: 'json',
-      type: 'POST',
-      data: formData,
-      success: function(data) {
-       if (confirm('Thank you for your data!')) {
-         this.setState({
-          name: '',
-          password: ''
-         })
-       }
-      },
-      error: function(xhr, status, err) {
-       console.error(status, err.toString())
-       alert('There was some problem with sending your data.')
-      }
-     })
-   
-     this.setState({
-      name: '',
-      password: ''
-     })
   }
 
 
@@ -60,33 +70,33 @@ export default class TestForm extends Component {
     return(
       <form onSubmit={this.handleSubmit}>
 
-      <fieldset className='form-group'>
-        <FormLabel htmlFor='formName' title='Full Name:' />
-        <input 
-          id='formName' 
-          className='form-input' 
-          name='name' 
-          type='text' 
-          required onChange={this.handleChange} 
-          value={this.state.name} 
-        />
-      </fieldset>
+        <fieldset className='form-group'>
+          <FormLabel htmlFor='formName' title='Full Name:' />
+          <input
+            id='formName'
+            className='form-input'
+            name='name'
+            type='text'
+            required onChange={this.handleChange}
+            value={this.state.name}
+          />
+        </fieldset>
 
-      <fieldset className='form-group'>
-        <FormLabel htmlFor='formPass' title='Password:' />
-        <input 
-          id='formName' 
-          className='form-input' 
-          name='password' 
-          type='password' 
-          required onChange={this.handleChange} 
-          value={this.state.name} 
-        />
-      </fieldset>
+        <fieldset className='form-group'>
+          <FormLabel htmlFor='formPass' title='Password:' />
+          <input
+            id='formName'
+            className='form-input'
+            name='password'
+            type='password'
+            required onChange={this.handleChange}
+            value={this.state.password}
+          />
+        </fieldset>
 
-      <div className='form-group'>
-        <input id='formButton' type='submit'/>
-      </div>
+        <div className='form-group'>
+          <input id='formButton' type='submit'/>
+        </div>
 
       </form>
       // <button onClick={this.clickedBtn}>Our First Test</button>
