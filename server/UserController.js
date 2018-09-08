8e9e3a6648ed999a1d68ca328a9dbd6af1ea02c5
Akouvi <kouedjinl@gmail.com>
2018-09-07 20:09:44 -0400

    added funciton to check if the user exists

--- server/UserController.js
+++ server/UserController.js
@@ -1,8 +1,20 @@
 const User = require("./UserModel");
 
 const UserController = {
+  checkDB(req, res, next) {
+    User.findOne(
+      { name: res.locals.name, password: res.locals.token },
+      (err, response) => {
+        if (response) {
+          res.send(response);
+        } else {
+          next();
+        }
+      }
+    );
+  },
   addUser(req, res) {
-    console.log("HELLO FROM USER");
+    console.log("HELLO FROM USER", req.body);
     const aUser = new User({
       name: "akouvi",
       password: "password"
@@ -11,22 +23,24 @@ const UserController = {
 
     aUser.save((err, aUser) => {
       if (err) {
+        res.status(404).send("user not found");
         console.log("this is error", err);
       }
       // console.log("another log of aUser", aUser);
-
-      res.send(aUser);
+      res.status(200).send(aUser);
     });
   },
   getUser(req, res) {
     User.findOne({ name: "akouvi" }, (err, user) => {
-      console.log("GETTING A USER");
+      // console.log("GETTING A USER");
       if (err) {
-        console.log(err);
-      } else {
-        console.log(user);
+        // console.log("we have an error trying to get user");
 
-        res.send(user);
+        res.status(404).send(err);
+      } else {
+        // console.log(user);
+
+        res.status(200).send(user);
       }
     });
   }
