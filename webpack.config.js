317536453b7409162de3707763f640907359f3ad
Den Temple <developer@dentemple.com>
2018-09-11 11:17:35 -0400

    Configure webpack with electron

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,45 @@
-const path = require('path');
+const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
-	devtool: 'inline-source-map',
-	mode: 'development',
-	entry: ['./src/index.js', 'webpack/hot/dev-server'],
-	output: {
-		path: path.resolve(__dirname, "build/"),
-		filename: 'bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader'
-			},
-			{
-				test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
-			},
-			{
-				test: /\.(png|svg|jpg|gif)$/,
-				use: ['file-loader']
-			}
-		]
-	},
-	devServer: {
-		contentBase: path.join(__dirname, 'public/'),
-		port: 3000,
-		publicPath: 'http://localhost:3000/build/',
-		historyApiFallback: false,
-		proxy: {
-      '/api': {
-          target: 'http://localhost:8080/',
-          secure: false
+  devtool: 'inline-source-map',
+  mode: 'development',
+  entry: ['./src/index.js', 'webpack/hot/dev-server'],
+  output: {
+    path: path.resolve(__dirname, 'build/'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
       }
-    },
-	},
-	resolve: {
-		extensions: ['*', '.js', '.jsx']
-	}
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public/'),
+    port: 3000,
+    publicPath: 'http://localhost:3000/build/',
+    historyApiFallback: false,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080/',
+        secure: false
+      }
+    }
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  plugins: [new webpack.IgnorePlugin(new RegExp('^(fs|ipc)$'))]
 }
