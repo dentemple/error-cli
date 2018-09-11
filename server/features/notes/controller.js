3c2c08380b810e6da964abddd0f5c9a82c034fda
Den Temple <developer@dentemple.com>
2018-09-11 19:52:28 -0400

    Add routes

---
+++ server/features/notes/controller.js
@@ -0,0 +1,19 @@
+const Notes = require('./model')
+
+function createNote(req, res, next) {}
+
+function getNote(req, res, next) {}
+
+function getAllNotes(req, res, next) {}
+
+function updateNote(req, res, next) {}
+
+function deleteNote(req, res, next) {}
+
+module.exports = {
+  createNote,
+  getNote,
+  getAllNotes,
+  updateNote,
+  deleteNote
+}
