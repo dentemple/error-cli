8030c4d7ed967e2ad409f5f83f2e7b68dfb9649d
Den Temple <developer@dentemple.com>
2018-09-12 11:52:05 -0400

    Fix broken git merge

--- server/server.js
+++ server/server.js
@@ -1,17 +1,7 @@
-<<<<<<< 919750fccd3074dda0221eebe10178550c037f15
-const SearchController = require("./SearchController");
-const UserController = require("./UserController");
-const userRouter = require('./userRouter');
-const bodyParser = require("body-parser");
-const express = require("express");
-const app = express();
-=======
-const SearchController = require('./SearchController')
-const UserController = require('./UserController')
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
->>>>>>> Add routes
+const UserController = require('./UserController')
 
 ///////////
 /*MongoDB*/
@@ -34,47 +24,23 @@ db.once('open', () => {
 ///////////////
 /*MiddleWare*/
 ///////////////
-<<<<<<< 8353ca75baf58607ce992e842730e3da67d6d3ab
-<<<<<<< 919750fccd3074dda0221eebe10178550c037f15
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-=======
-=======
 app.use(
   bodyParser.urlencoded({
     extended: true
   })
 )
->>>>>>> Add routes and configuration
 app.use(bodyParser.json())
->>>>>>> Add routes
 
 ///////////
 /*Routes*/
 ///////////
-<<<<<<< 8353ca75baf58607ce992e842730e3da67d6d3ab
-<<<<<<< 919750fccd3074dda0221eebe10178550c037f15
+app.use('/users', userRouter)
 
-app.use("/users", userRouter)
+app.get('/api', (req, res) => {
+  res.send('up and running')
+})
 
-app.get("/api", (req, res) => {
-  res.send("up and running");
-});
-
-// app.get("/api/login", UserController.authenticateUser);
-
-// app.get("/api/oauth",
-//   UserController.handleAthenticatedUser,
-//   UserController.getAuthInfo,
-//   UserController.checkDB,
-//   UserController.addUser
-// );
-=======
-const notesRoutes = require('./notes/routes.js')
-app.use('/notes', notesRoutes)
-=======
 app.use('/notes', require('./notesFeature/routes.js'))
->>>>>>> Add routes and configuration
 
 app.get('/api', (req, res) => {
   res.send('up and running')
@@ -94,6 +60,5 @@ app.get(
   UserController.checkDB,
   UserController.addUser
 )
->>>>>>> Add routes
 
 app.listen(8080, () => console.log('🚦 Now listening on port 8080 🚦'))
