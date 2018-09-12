3e2ced0f2b2ea65882b896a0077c2824923e8c13
Den Temple <developer@dentemple.com>
2018-09-12 11:46:58 -0400

    Resolve merge conflicts

--- server/server.js
+++ server/server.js
@@ -1,34 +1,50 @@
+<<<<<<< 919750fccd3074dda0221eebe10178550c037f15
 const SearchController = require("./SearchController");
 const UserController = require("./UserController");
 const userRouter = require('./userRouter');
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
+=======
+const SearchController = require('./SearchController')
+const UserController = require('./UserController')
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+>>>>>>> Add routes
 
 ///////////
 /*MongoDB*/
 ///////////
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/ecli_db', { useNewUrlParser: true });
+mongoose.connect(
+  'mongodb://localhost:27017/ecli_db',
+  { useNewUrlParser: true }
+)
 
-const db = mongoose.connection;
+const db = mongoose.connection
 
-db.on("error", console.error.bind(console, "connection error"));
+db.on('error', console.error.bind(console, 'connection error'))
 
-db.once("open", () => {
-  console.log("Connected to the ecli_db local database");
-});
+db.once('open', () => {
+  console.log('Connected to the ecli_db local database')
+})
 
 ///////////////
 /*MiddleWare*/
 ///////////////
+<<<<<<< 919750fccd3074dda0221eebe10178550c037f15
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+=======
+app.use(bodyParser.json())
+>>>>>>> Add routes
 
 ///////////
 /*Routes*/
 ///////////
+<<<<<<< 919750fccd3074dda0221eebe10178550c037f15
 
 app.use("/users", userRouter)
 
@@ -44,5 +60,28 @@ app.get("/api", (req, res) => {
 //   UserController.checkDB,
 //   UserController.addUser
 // );
+=======
+const notesRoutes = require('./notes/routes.js')
+app.use('/notes', notesRoutes)
 
-app.listen(8080, () => console.log("🚦 Now listening on port 8080 🚦"));
+app.get('/api', (req, res) => {
+  res.send('up and running')
+})
+
+app.get('/testing', (req, res) => {
+  console.log('hit the server')
+  res.send('You got it DUDE!!!')
+})
+
+app.get('/api/login', UserController.authenticateUser)
+
+app.get(
+  '/api/oauth',
+  UserController.handleAthenticatedUser,
+  UserController.getAuthInfo,
+  UserController.checkDB,
+  UserController.addUser
+)
+>>>>>>> Add routes
+
+app.listen(8080, () => console.log('🚦 Now listening on port 8080 🚦'))
